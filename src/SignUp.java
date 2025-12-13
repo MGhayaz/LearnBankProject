dc70bb44698b5db6ae0abc23bfa0aa6c96e31ee5
Ghayaz <mohammedghayaz949@gmail.com>
2025-12-13 13:20:12 +0530

    FUNCTIONAL GENDER,DATE,MARITAL BUTTONS

--- src/SignUp.java
+++ src/SignUp.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import com.toedter.calendar.JDateChooser;
 
 public class SignUp extends JFrame {
 
@@ -48,11 +49,42 @@ public class SignUp extends JFrame {
         DOB.setBounds(90, 320, 150, 25);
         DOB.setFont(new Font("Raleway", Font.BOLD, 18));
         add(DOB);
+
+        JDateChooser DOBFeild = new JDateChooser();
+        DOBFeild.setBounds(250, 320, 400, 25);
+        DOBFeild.setForeground(Color.BLACK);
+        add(DOBFeild);
         //gender
         JLabel Gender = new JLabel("Gender:");
         Gender.setBounds(90, 370, 80, 25);
         Gender.setFont(new Font("Raleway", Font.BOLD, 18));
         add(Gender);
+
+        JRadioButton male = new JRadioButton("MALE");
+        male.setBounds(250, 370, 80, 25);
+        male.setForeground(Color.BLACK);
+        male.setBackground(Color.WHITE);
+        male.setFont(new Font("Raleway",Font.PLAIN, 18));
+        add(male);
+
+        JRadioButton female = new JRadioButton("FEMALE");
+        female.setBounds(370, 370, 100, 25);
+        female.setForeground(Color.BLACK);
+        female.setBackground(Color.WHITE);
+        female.setFont(new Font("Raleway",Font.PLAIN, 18));
+        add(female);
+
+        JRadioButton othergender = new JRadioButton("OTHER");
+        othergender.setBounds(500, 370, 90, 25);
+        othergender.setForeground(Color.BLACK);
+        othergender.setBackground(Color.WHITE);
+        othergender.setFont(new Font("Raleway",Font.PLAIN, 18));
+        add(othergender);
+
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(male);
+        buttonGroup.add(female);
+        buttonGroup.add(othergender);
         //email
         JLabel Email = new JLabel("Email Address:");
         Email.setBounds(90, 420, 150, 25);
@@ -67,6 +99,30 @@ public class SignUp extends JFrame {
         Maternity.setBounds(90, 470, 140, 25);
         Maternity.setFont(new Font("Raleway", Font.BOLD, 18));
         add(Maternity);
+        JRadioButton Married = new JRadioButton("MARRIED");
+        Married.setBounds(250, 470, 120, 25);
+        Married.setForeground(Color.BLACK);
+        Married.setBackground(Color.WHITE);
+        Married.setFont(new Font("Raleway",Font.PLAIN, 18));
+        add(Married);
+
+        JRadioButton UnMarried = new JRadioButton("UNMARRIED");
+        UnMarried.setBounds(370, 470, 140, 25);
+        UnMarried.setForeground(Color.BLACK);
+        UnMarried.setBackground(Color.WHITE);
+        UnMarried.setFont(new Font("Raleway",Font.PLAIN, 18));
+        add(UnMarried);
+        JRadioButton otherMaternityStatus = new JRadioButton("OTHER");
+        otherMaternityStatus.setBounds(520, 470, 90, 25);
+        otherMaternityStatus.setForeground(Color.BLACK);
+        otherMaternityStatus.setBackground(Color.WHITE);
+        otherMaternityStatus.setFont(new Font("Raleway",Font.PLAIN, 18));
+        add(otherMaternityStatus);
+
+        ButtonGroup MaritalbuttonGroup = new ButtonGroup();
+        MaritalbuttonGroup.add(Married);
+        MaritalbuttonGroup.add(UnMarried);
+        MaritalbuttonGroup.add(otherMaternityStatus);
         //address
         JLabel Address = new JLabel("Address:");
         Address.setBounds(90, 520, 80, 25);
